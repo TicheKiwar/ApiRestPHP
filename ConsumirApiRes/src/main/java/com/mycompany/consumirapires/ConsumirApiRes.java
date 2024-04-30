17a23e5a3f40f00c4ffa54ab185139af4d3b9b2e
TicheKiwar <kiwartiche@gmail.com>
2024-04-30 12:48:40 -0500

    Create Project java desktop consumer ApiRest to php

---
+++ ConsumirApiRes/src/main/java/com/mycompany/consumirapires/ConsumirApiRes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.consumirapires;
+
+/**
+ *
+ * @author Kiwar
+ */
+public class ConsumirApiRes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ConsumirApiRes/src/main/java/com/mycompany/consumirapires/CrudRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.consumirapires;
+
+import java.util.List;
+
+/**
+ *
+ * @author Kiwar
+ */
+public interface CrudRepository <T>{
+    List<T> getAll();
+    boolean create(T entity);
+    boolean update(T entity);
+    boolean delete(String id);
+}
